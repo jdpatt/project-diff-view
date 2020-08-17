20fe849aeb26bbdd52d3354c75489fd935eca648
David <dpatter877@gmail.com>
2020-08-17 19:23:45 -0400

    dataclass doesn't use __dict__

--- projectdiffview/configuration.py
+++ projectdiffview/configuration.py
@@ -1,7 +1,6 @@
 """Default settings or constants for projectdiffview."""
 import filecmp
 import logging
-from dataclasses import dataclass
 from pathlib import Path
 
 from PySide2 import QtCore, QtGui
@@ -9,24 +8,25 @@ from PySide2 import QtCore, QtGui
 LOG = logging.getLogger("projectdiffview.config")
 
 
-@dataclass
 class Configuration:
     """Container to store settings for ProjectDiffView."""
+    def __init__(self):
+        self.template_directory = Path(__file__).parents[1].joinpath("template")
+        self.debug_template_directory = Path(__file__).parents[1].joinpath("tests", "data", "template")
+        self.ignored = filecmp.DEFAULT_IGNORES + [".keep"]
 
-    template_directory = Path(__file__).parents[1].joinpath("template")
-    debug_template_directory = Path(__file__).parents[1].joinpath("tests", "data", "template")
-    ignored = filecmp.DEFAULT_IGNORES + [".keep"]
-
-    color_deleted = QtGui.QColor(255, 162, 137)
-    color_new = QtGui.QColor(68, 188, 145)
-    color_added = QtGui.QColor(27, 157, 226)
+        self.color_deleted = QtGui.QColor(255, 162, 137)
+        self.color_new = QtGui.QColor(68, 188, 145)
+        self.color_added = QtGui.QColor(27, 157, 226)
 
     def load_config(self):
         """Load the settings into ProjectDiffView."""
         settings = QtCore.QSettings()
         LOG.info("Loading Configuration...")
         for item in self.__dict__:
-            self.__dict__[item] = settings.value(item)
+            value = settings.value(item)
+            if value:
+                self.__dict__[item] = value
 
     def save_config(self):
         """Save the settings out from ProjectDiffView."""
